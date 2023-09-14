ad037e49952838996b74e5be5eb2c9c081a6df2d
cheikhnouha <nouhafallbarro@gmail.com>
2023-09-14 11:17:16 +0000

    ajout multiplication

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
+/* ajput function*/
 const add = (num1, num2) => {
  let result = num1 - num2;
  return result;
 }
 const nou = [300,2,300];
 console.log(nou.map(add));
+
+/* muultiply function */
+
+const multiply = (num1, num2) => {
+ let resul = num1 * num2;
+ return resul;
+}
+ const con = [ 1234,566]
+ console.log(con.map(multiply));
